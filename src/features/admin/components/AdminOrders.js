ad58954b6bd073a1faaedb65813c700531016e5f
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2023-08-22 06:44:09 +0530

    Admin Panel Page Completed along with some sorting options added

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -7,7 +7,8 @@ import {
   selectTotalOrders,
   updateOrderAsync,
 } from "../../order/orderSlice";
-import { PencilIcon, EyeIcon } from '@heroicons/react/24/outline';
+import { PencilIcon, EyeIcon, ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
+import Pagination from "../../common/Pagination";
 
 function AdminOrders() {
   const [page, setPage] = useState(1);
@@ -15,40 +16,51 @@ function AdminOrders() {
   const orders = useSelector(selectOrders);
   const totalOrders = useSelector(selectTotalOrders);
   const [editableOrderId, setEditableOrderId] = useState(-1);
+  const [sort, setSort] = useState({});
 
-  const handleEdit =(order)=>{
+  const handleEdit = (order) => {
     setEditableOrderId(order.id);
-  }
+  };
 
-  const handleShow =()=>{
-    console.log("handleShow")
-  }
+  const handleShow = () => {
+    console.log("handleShow");
+  };
 
-  const handleUpdate = (e,order)=>{
-    const updatedOrder = {...order, status:e.target.value}
+  const handleUpdate = (e, order) => {
+    const updatedOrder = { ...order, status: e.target.value };
     dispatch(updateOrderAsync(updatedOrder));
     setEditableOrderId(-1);
   };
 
-  const chooseColor = (status)=>{
-    switch(status){
-      case 'pending':
-        return 'bg-purple-200 text-purple-600';
-      case 'dispatched':
-        return 'bg-yellow-200 text-yellow-600';
-      case 'delivered':
-        return 'bg-green-200 text-green-600';
-      case 'cancelled':
-        return 'bg-red-200 text-red-600';
+  const handlePage = (page) => {
+    setPage(page);
+  };
+
+  const handleSort = (sortOption) => {
+    const sort = { _sort: sortOption.sort, _order: sortOption.order };
+    console.log({ sort });
+    setSort(sort);
+  };
+
+  const chooseColor = (status) => {
+    switch (status) {
+      case "pending":
+        return "bg-purple-200 text-purple-600";
+      case "dispatched":
+        return "bg-yellow-200 text-yellow-600";
+      case "delivered":
+        return "bg-green-200 text-green-600";
+      case "cancelled":
+        return "bg-red-200 text-red-600";
       default:
-        return 'bg-purple-200 text-purple-600';
+        return "bg-purple-200 text-purple-600";
     }
-  }
+  };
 
   useEffect(() => {
     const pagination = { _page: page, _limit: ITEMS_PER_PAGE };
-    dispatch(fetchAllOrdersAsync(pagination));
-  }, [dispatch, page]);
+    dispatch(fetchAllOrdersAsync({ sort,pagination }));
+  }, [dispatch, page, sort]);
 
   return (
     <div class="overflow-x-auto">
@@ -61,10 +73,36 @@ function AdminOrders() {
                 <table className="min-w-max w-full table-auto">
                   <thead>
                     <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                      <th className="py-3 px-6 text-left">Order#</th>
+                      <th
+                        className="py-3 px-6 text-left cursor-pointer"
+                        onClick={(e) =>
+                          handleSort({
+                            sort: "id",
+                            order: sort?._order === "asc" ? "desc" : "asc",
+                          })
+                        }
+                      >
+                        Order# {' '}
+                        {sort._sort ==='id' && (sort._order ==='asc' ?  (<ArrowUpIcon className ='w-4 h-4 inline'></ArrowUpIcon>) :
+                        (<ArrowDownIcon className="w-4 h-4 inline"></ArrowDownIcon>))}
+                      </th>
                       <th className="py-3 px-6 text-left">Items</th>
-                      <th className="py-3 px-6 text-center">Total Amount</th>
-                      <th className="py-3 px-6 text-center">Shipping Address</th>
+                      <th
+                        className="py-3 px-6 text-left cursor-pointer"
+                        onClick={(e) =>
+                          handleSort({
+                            sort: "totalAmount",
+                            order: sort?._order === "asc" ? "desc" : "asc",
+                          })
+                        }
+                      >
+                        Total Amount {' '}
+                        {sort._sort ==='totalAmount' && (sort._order ==='asc' ?  (<ArrowUpIcon className ='w-4 h-4 inline'></ArrowUpIcon>) :
+                        (<ArrowDownIcon className="w-4 h-4 inline"></ArrowDownIcon>))}
+                      </th>
+                      <th className="py-3 px-6 text-center">
+                        Shipping Address
+                      </th>
                       <th className="py-3 px-6 text-center">Status</th>
                       <th className="py-3 px-6 text-center">Actions</th>
                     </tr>
@@ -87,7 +125,10 @@ function AdminOrders() {
                                   src={item.thumbnail}
                                 />
                               </div>
-                              <span>{item.title} - #{item.quantity} - ${discountedPrice(item)}</span>
+                              <span>
+                                {item.title} - #{item.quantity} - $
+                                {discountedPrice(item)}
+                              </span>
                             </div>
                           ))}
                         </td>
@@ -98,7 +139,9 @@ function AdminOrders() {
                         </td>
                         <td className="py-3 px-6 text-center">
                           <div className="items-center justify-center">
-                            <div><strong>{order.selectedAddress.name}</strong>,</div>
+                            <div>
+                              <strong>{order.selectedAddress.name}</strong>,
+                            </div>
                             <div>{order.selectedAddress.street},</div>
                             <div>{order.selectedAddress.city},</div>
                             <div>{order.selectedAddress.pinCode},</div>
@@ -107,24 +150,37 @@ function AdminOrders() {
                           </div>
                         </td>
                         <td className="py-3 px-6 text-center">
-                          {order.id === editableOrderId ? (<select onChange={e=>handleUpdate(e,order)}>
-                          <option value="pending">Pending</option>
-                          <option value="dispatched">Dispatched</option>
-                          <option value="delivered">Delivered</option>
-                          <option value="cancelled">Cancelled</option>
-                          </select>) : (<span className={`${chooseColor(order.status)} py-1 px-3 rounded-full text-xs`}>
-                            {order.status}
-                          </span>)}
+                          {order.id === editableOrderId ? (
+                            <select onChange={(e) => handleUpdate(e, order)}>
+                              <option value="pending">Pending</option>
+                              <option value="dispatched">Dispatched</option>
+                              <option value="delivered">Delivered</option>
+                              <option value="cancelled">Cancelled</option>
+                            </select>
+                          ) : (
+                            <span
+                              className={`${chooseColor(
+                                order.status
+                              )} py-1 px-3 rounded-full text-xs`}
+                            >
+                              {order.status}
+                            </span>
+                          )}
                         </td>
                         <td className="py-3 px-6 text-center">
                           <div className="flex item-center justify-center">
                             <div className="w-6 mr-4 transform hover:text-purple-500 hover:scale-110">
-                              <EyeIcon className="w-8 h-8" onClick={e=>handleShow(order)}></EyeIcon>
+                              <EyeIcon
+                                className="w-8 h-8"
+                                onClick={(e) => handleShow(order)}
+                              ></EyeIcon>
                             </div>
                             <div className="w-6 mr-2 transform hover:text-purple-500 hover:scale-110">
-                              <PencilIcon className="w-8 h-8" onClick={e=>handleEdit(order)}></PencilIcon>
+                              <PencilIcon
+                                className="w-8 h-8"
+                                onClick={(e) => handleEdit(order)}
+                              ></PencilIcon>
                             </div>
-
                           </div>
                         </td>
                       </tr>
@@ -134,6 +190,12 @@ function AdminOrders() {
               </div>
             </div>
           </div>
+          <Pagination
+            page={page}
+            setPage={setPage}
+            handlePage={handlePage}
+            totalItems={totalOrders}
+          ></Pagination>
         </div>
       </>
     </div>
--- src/features/order/orderAPI.js
+++ src/features/order/orderAPI.js
@@ -25,8 +25,12 @@ export function updateOrder(order) {
   );
 }
 
-export function fetchAllOrders(pagination) {
+export function fetchAllOrders(sort,pagination) {
   let queryString = '';
+
+  for(let key in sort ){
+    queryString += `${key}=${sort[key]}&`;
+  }
   for(let key in pagination){
     queryString += `${key}=${pagination[key]}&`
   }
